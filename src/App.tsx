da3bcfbeaf5d1f559aeeda43d992cacf322f136d
Nick Clifford <nick@nickclifford.me>
2022-04-30 17:03:52 -0500

    base MUI skeleton, prettier setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { AppBar, Box, Button, Card, CardActions, CardContent, Toolbar, Typography } from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<AppBar position="static">
+				<Toolbar>
+					<Typography variant="h5">Spotify Data Viewer</Typography>
+				</Toolbar>
+			</AppBar>
+			<Box
+				sx={{
+					// fill space from root flexbox
+					flexGrow: 1,
+					// center children
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+				}}
+			>
+				<Card elevation={5} sx={{ maxWidth: "20%" }}>
+					<CardContent>
+						<Typography variant="h5">Welcome to the Spotify Data Viewer!</Typography>
+						<Typography variant="body1">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+							labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+							laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+							voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+							cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</Typography>
+					</CardContent>
+					<CardActions>
+						<Button>Get Started</Button>
+					</CardActions>
+				</Card>
+			</Box>
+		</>
+	);
 }
 
 export default App;
