40362af5e3bdcd6e2637d8b83c7508194a4f97e3
Nick Clifford <nick@nickclifford.me>
2022-05-17 14:50:48 -0500

    JSON schema, example how to use DataSource

--- src/Explorer.tsx
+++ src/Explorer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import DataSource from "./dataSource";
-import { Card, CardContent } from "@mui/material";
+import { Box, Card, CardContent, List, ListItem, ListItemText, Typography } from "@mui/material";
+import { MusicNote } from "@mui/icons-material";
 
 interface ExplorerProps {
 	source: DataSource;
@@ -9,7 +10,37 @@ interface ExplorerProps {
 export default function Explorer({ source }: ExplorerProps) {
 	return (
 		<Card>
-			<CardContent>You have the following categories of data: {source.categories.join(", ")}</CardContent>
+			<CardContent>
+				{source.categories.includes("Playlist1") ? (
+					<>
+						<Typography variant="h5">Playlists</Typography>
+						<List>
+							{source.getCategory("Playlist1").playlists.map((p, i) => (
+								<ListItem
+									key={i}
+									secondaryAction={
+										<Box
+											sx={{
+												display: "flex",
+												flexDirection: "column",
+											}}
+										>
+											<Typography variant="caption" align="center">
+												{p.items.length}
+											</Typography>
+											<MusicNote />
+										</Box>
+									}
+								>
+									<ListItemText primary={p.name} secondary={p.description} />
+								</ListItem>
+							))}
+						</List>
+					</>
+				) : (
+					`You have the following data categories: ${source.categories.join(", ")}`
+				)}
+			</CardContent>
 		</Card>
 	);
 }
--- src/dataSource.ts
+++ src/dataSource.ts
@@ -1,4 +1,66 @@
-// TODO: schema for real data, better typed getCategory
+export interface Playlist {
+	name: string;
+	lastModifiedDate: string; // YYYY-MM-DD
+	items: Array<{
+		track: Record<"trackName" | "artistName" | "albumName" | "trackUri", string>;
+		episode: string | null; // if podcast, name of episode
+		localTrack: string | null; // if local file, name of track
+	}>;
+	description: string;
+	numberOfFollowers: number;
+}
+export interface SingleStream {
+	endTime: string; // YYYY-MM-DD HH:mm
+	artistName: string;
+	trackName: string;
+	msPlayed: number;
+}
+export interface Library {
+	tracks: Array<Record<"artist" | "album" | "track" | "uri", string>>;
+	albums: Array<Record<"artist" | "album" | "uri", string>>;
+	artist: Array<Record<"name" | "uri", string>>;
+	// TODO: these arrays have no entries in my dump
+	shows: never[];
+	episodes: never[];
+	bannedTracks: never[];
+	bannedArtists: never[];
+	other: never[];
+}
+export interface SingleSearch {
+	platform: string;
+	searchTime: string; // YYYY-MM-DDTHH:mm:ss.SSSZ[UTC]
+	searchQuery: string;
+	searchInteractionURIs: string[];
+}
+export interface Follow {
+	followerCount: number;
+	followingUsersCount: number;
+	dismissingUsersCount: number;
+}
+export interface UserData {
+	username: string;
+	email: string;
+	country: string;
+	createdFromFacebook: boolean;
+	facebookUid: string | null; // TODO this is a guess, my dump is just null
+	birthdate: string; // YYYY-MM-dd
+	gender: string;
+	postalCode: string;
+	mobileNumber: string;
+	mobileOperator: string;
+	mobileBrand: string;
+	creationTime: string; // YYYY-MM-dd
+}
+
+export interface DataMap {
+	Playlist1: { playlists: Playlist[] };
+	StreamingHistory0: SingleStream[];
+	YourLibrary: Library;
+	SearchQueries: SingleSearch[];
+	Follow: Follow;
+	Userdata: UserData;
+	Inferences: { inferences: string[] };
+}
 
 export default class DataSource {
 	// unparsed raw strings
@@ -10,13 +72,13 @@ export default class DataSource {
 		this.unparsed[category] = jsonSource;
 	}
 
-	getCategory(category: string): any {
+	getCategory<K extends keyof DataMap>(category: K): DataMap[K] {
 		let value = this.parsed[category];
 
 		if (typeof value === "undefined") {
 			value = JSON.parse(this.unparsed[category]);
 			delete this.unparsed[category];
-			this.parsed = value;
+			this.parsed[category] = value;
 		}
 
 		return value;
